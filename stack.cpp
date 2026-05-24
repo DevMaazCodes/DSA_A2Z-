bbb89641f0454989fa4b3ebee0616a80c9338393
DevMaazCodes <maaz79800@gmail.com>
2026-05-24 22:46:26 +0530

    added swap to stacks's note

--- stack.cpp
+++ stack.cpp
@@ -1,5 +1,5 @@
 //stack follows last in first out (LIFO). It is a container and syntax is similar to vectors. But there's no indexing in it.
-//stack only has 3 major functions, stack.push/emplace, .pop, .top , the basic functions like size and empty stays same.
+//stack only has 3 major functions, stack.push/emplace, .pop, .top , the basic functions like size and emptyand swap stays same.
 #include <iostream>
 #include <stack>
 int main(){
@@ -11,5 +11,12 @@ int main(){
     std::cout<<st.top(); //cout 5.
     st.pop(); //pop i.e delete the top at the stack
     std::cout<<st.top(); //cout 4.
+    std::stack<int>st1;
+    st1.push(6);
+    st1.push(7);
+    st1.push(8); 
+    st1.emplace(9);
+    st1.swap(st);
+    std::cout<<st.top(); //cout 9 which was the top of st1 successfully swapped
     return 0;
 }
