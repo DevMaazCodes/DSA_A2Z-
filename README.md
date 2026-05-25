bceccb0663da5d5451346bb39ff6a928f0640c09
DevMaazCodes <maaz79800@gmail.com>
2026-05-25 13:55:34 +0530

    learned basic multisets

--- README.md
+++ README.md
@@ -19,6 +19,7 @@ In this repo, I will be tracking my progress of DSA learning and practicing unde
 - Basics of Queue under STL
 - Basics of Priority Queue under STL
 - Basics of Sets under STL
+- Basics of MultiSets under STL
 
 ## Questions Solved
 - 19 Patterns
---
+++ multiset.cpp
@@ -0,0 +1,16 @@
+//a multiset is same as set but the elements are not required to be unique, it can store many occurances of same element
+#include <bits/stdc++.h>
+int main(){
+    std::multiset<int>x;
+    x.insert(7);
+    x.insert(5);
+    x.insert(8);
+    x.insert(8);
+    x.insert(8);
+    x.insert(6);
+    x.insert(2);
+    x.erase(x.find(8)); //this erases only one of the eights as .find function gives a memory address to the first two
+    x.erase(8); //this erases all of the eights.
+    
+    return 0;
+}
