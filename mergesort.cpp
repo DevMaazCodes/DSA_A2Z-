addbc582db3172580906558f09705c6da61ee19f
DevMaazCodes <maaz79800@gmail.com>
2026-06-03 17:03:55 +0530

    merge sort started its kinda difficult

---
+++ mergesort.cpp
@@ -0,0 +1,7 @@
+//Merge sort is a very efficient recursive sorting algo, it follows divide and merge rule.
+#include <bits/stdc++.h>
+int main(){
+    int arr[]={1,4,3,6,5,7,8,9,0,2};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    return 0;
+}
