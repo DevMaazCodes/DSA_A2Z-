f5b9068d68a8ea77e23740884c982152ef27c3ff
DevMaazCodes <maaz79800@gmail.com>
2026-06-19 21:03:16 +0530

    array hard q2 file

---
+++ arrayhard2.cpp
@@ -0,0 +1,4 @@
+//we will majority element II here
+//the brute is simple nested loops checking which element appears more than n/3 times
+//the better is hashing and checking when element's counter reaches >n/3 times and as soon as 2 elements are there we return
+//the best is extended moore's voting algorithmas done below
